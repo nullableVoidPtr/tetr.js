4998eb6b1b006db7e83ff62822bc9aaf09dc1c06
nullableVoidPtr <30564701+nullableVoidPtr@users.noreply.github.com>
2019-10-06 21:08:32 +0800

    Classes

--- hold.js
+++ hold.js
@@ -1,22 +1,24 @@
-function Hold() {
-  this.piece;
+class Hold {
+	constructor(context) {
+		this.piece = void 0;
+		this.context = context;
+	}
+	draw() {
+		clear(this.context);
+		if (this.piece === 0 || this.piece === 3) {
+			draw(
+				pieces[this.piece].tetro,
+				pieces[this.piece].x - 3,
+				2 + pieces[this.piece].y,
+				this.context,
+			);
+		} else {
+			draw(
+				pieces[this.piece].tetro,
+				pieces[this.piece].x - 2.5,
+				2 + pieces[this.piece].y,
+				this.context,
+			);
+		}
+	}
 }
-Hold.prototype.draw = function() {
-  clear(holdCtx);
-  if (this.piece === 0 || this.piece === 3) {
-    draw(
-      pieces[this.piece].tetro,
-      pieces[this.piece].x - 3,
-      2 + pieces[this.piece].y,
-      holdCtx,
-    );
-  } else {
-    draw(
-      pieces[this.piece].tetro,
-      pieces[this.piece].x - 2.5,
-      2 + pieces[this.piece].y,
-      holdCtx,
-    );
-  }
-};
-var hold = new Hold();
